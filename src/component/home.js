95f1c7484c62afd242ed0a0cd087ab840bf94aef
Pulok <pulok@property.credit>
2023-09-12 21:00:04 +1000

    Home page & Routing

---
+++ src/component/home.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./home.css";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <header className="home-header">
+        <h1>Welcome to the Incident Reporting System</h1>
+        <p>Report incidents easily and help maintain safety.</p>
+      </header>
+      <nav className="navbar">
+        <Link to="/form.js" className="nav-link">
+          Report an Incident
+        </Link>
+        <Link to="/login.js" className="nav-link">
+          Login
+        </Link>
+        <Link to="/archive.js" className="nav-link">
+          Archive
+        </Link>
+      </nav>
+    </div>
+  );
+};
+export default Home;
