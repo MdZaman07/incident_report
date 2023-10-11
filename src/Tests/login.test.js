788104eee33d5d1dfcbfe95985e0efc3e131c84c
Darryl Neo <darrylnck72@gmail.com>
2023-10-11 11:02:51 +1100

    Testing solution to fix login Test

--- src/Tests/login.test.js
+++ src/Tests/login.test.js
@@ -1,6 +1,11 @@
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom'; // Import MemoryRouter
 import Login from '../component/login';
 
 test('renders form', () => {
-    render(<Login/>);
-  });
+  render(
+    <MemoryRouter>
+      <Login />
+    </MemoryRouter>
+  );
+});
