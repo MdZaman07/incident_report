030df0566269a0c4aea15efc80f2f29bf45da2c2
byro23 <byron.j.lester@student.uts.edu.au>
2023-10-12 21:30:48 +1100

    Update form.test.js

--- src/Tests/form.test.js
+++ src/Tests/form.test.js
@@ -1,6 +1,34 @@
-import { render } from '@testing-library/react';
+import {
+  render,
+  fireEvent,
+  waitFor,
+  getByDisplayValue,
+} from "@testing-library/react";
 import Form from '../component/form';
 
 test('renders form', () => {
     render(<Form/>);
   });
+
+test('text fields update', async () => {
+  const {container} = render(<Form/>)
+
+  const textInput = container.querySelector("#incidentTitle");
+
+  fireEvent.change(textInput, { target: { value: "Test incident title" } });
+  expect(textInput.value).toBe("Test incident title");
+
+})
+
+test('submit button status update', async () => {
+  const {container} = render(<Form/>)
+  const submitButton = container.querySelector("button[type='submit']");
+
+  await waitFor(() => {
+    fireEvent.click(submitButton)
+    
+  });
+
+
+
+})
