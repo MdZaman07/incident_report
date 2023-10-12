0b926b1751cc58515b98b09d050cfd1c8363cbb2
Derrick <xreamic@gmail.com>
2023-10-12 14:26:16 +1100

    update archive.js

--- src/component/archive.js
+++ src/component/archive.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 import "./archive.css";
 import { useNavigate } from "react-router-dom";
-import { mockData } from "./MOCKDATA.js";
 
 export default function Archive() {
   const navigate = useNavigate();
+  const [incidents, setIncidents] = useState([]);
+
+  const fetchIncidents = async () => {
+    const response = await fetch("http://localhost:4000/api/incidents");
+    const data = await response.json();
+    setIncidents(data);
+  };
+
+  useEffect(() => {
+    fetchIncidents();
+  }, []);
 
   const onRowClick = (event) => {
     const incidentID = event.data.incidentID;
@@ -16,24 +26,40 @@ export default function Archive() {
   return (
     <div className="centered-content">
       <DataTable
-        value={mockData}
+        value={incidents}
         paginator
         rows={10}
-        sortField="status"
         onRowClick={onRowClick}
       >
-        <Column field="incidentID" header="ID" sortable></Column>
-        <Column field="date" header="Date" sortable></Column>
-        <Column field="incidentTitle" header="Incident" sortable></Column>
+        <Column
+          field="date"
+          header="Date"
+          sortable
+          body={(rowData) => {
+            const date = new Date(rowData.date);
+            const day = date.getDate().toString().padStart(2, "0");
+            const month = (date.getMonth() + 1).toString().padStart(2, "0");
+            const year = date.getFullYear().toString().slice(-2);
+            return `${day}/${month}/${year}`;
+          }}
+        ></Column>
+        <Column field="incidentTitle" header="Incident Title" sortable></Column>
+        <Column
+          field="offenderName"
+          header="Offender Name"
+          sortable
+          body={(rowData) => {
+            const offenderName = rowData.offenderName || "Unknown";
+            return offenderName;
+          }}
+        ></Column>
         <Column
           field="incidentCategory"
           header="Incident Category"
           sortable
         ></Column>
-        <Column field="witnessName" header="Witness Name" sortable></Column>
-        <Column field="offenderName" header="Offender Name" sortable></Column>
+
         <Column field="status" header="Status" sortable></Column>
-        <Column field="severity" header="Status" sortable></Column>
       </DataTable>
     </div>
   );
