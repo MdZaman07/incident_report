d6e302d0e4282121fcaa922e61400b833b6025c7
Derrick <xreamic@gmail.com>
2023-10-12 15:25:26 +1100

    Update searchForms.test.js

--- src/Tests/searchForms.test.js
+++ src/Tests/searchForms.test.js
@@ -5,7 +5,7 @@ import {
   getByDisplayValue,
 } from "@testing-library/react";
 import SearchIncidents from "../component/searchForms";
-import { MemoryRouter } from "react-router-dom"; // Import MemoryRouter
+import { MemoryRouter } from "react-router";
 
 test("renders form", () => {
   render(
@@ -24,6 +24,7 @@ test("displays search criteria dropdown", () => {
   const searchInput = container.querySelector("#search-criteria");
   expect(searchInput).toBeInTheDocument();
 });
+
 test("displays search input field", () => {
   const { container } = render(
     <MemoryRouter>
@@ -33,6 +34,7 @@ test("displays search input field", () => {
   const searchInput = container.querySelector("#search-term");
   expect(searchInput).toBeInTheDocument();
 });
+
 test("updates search term when typing", () => {
   const { container } = render(
     <MemoryRouter>
@@ -40,4 +42,7 @@ test("updates search term when typing", () => {
     </MemoryRouter>
   );
   const searchInput = container.querySelector("#search-term");
+
   fireEvent.change(searchInput, { target: { value: "Test Search Term" } });
+  expect(searchInput.value).toBe("Test Search Term");
+});
