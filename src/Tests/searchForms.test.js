61ebb7d871c540321dc59da9e2a8e061e249d66b
byro23 <byron.j.lester@student.uts.edu.au>
2023-10-12 12:46:11 +1100

    Update searchForms.test.js

--- src/Tests/searchForms.test.js
+++ src/Tests/searchForms.test.js
@@ -14,19 +14,19 @@ test("renders form", () => {
 });
 
 test("displays search criteria dropdown", () => {
-  const { container } = render(<SearchIncidents />);
+  const { container } = <MemoryRouter>render(<SearchIncidents />)</MemoryRouter>;
   const searchInput = container.querySelector("#search-criteria");
   expect(searchInput).toBeInTheDocument();
 });
 
 test("displays search input field", () => {
-  const { container } = render(<SearchIncidents />);
+  const { container } = <MemoryRouter>render(<SearchIncidents />)</MemoryRouter>;
   const searchInput = container.querySelector("#search-term");
   expect(searchInput).toBeInTheDocument();
 });
 
 test("updates search term when typing", () => {
-  const { container } = render(<SearchIncidents />);
+  const { container } = <MemoryRouter>render(<SearchIncidents />)</MemoryRouter>;
   const searchInput = container.querySelector("#search-term");
 
   fireEvent.change(searchInput, { target: { value: "Test Search Term" } });
