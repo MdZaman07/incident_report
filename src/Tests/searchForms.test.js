9d4dc490ed163abeaf2116d2b44bdde4df98bb51
Derrick <xreamic@gmail.com>
2023-10-12 15:16:21 +1100

    fixed error

--- src/Tests/searchForms.test.js
+++ src/Tests/searchForms.test.js
@@ -5,28 +5,42 @@ import {
   getByDisplayValue,
 } from "@testing-library/react";
 import SearchIncidents from "../component/searchForms";
-import { MemoryRouter } from 'react-router-dom'; // Import MemoryRouter
+import { MemoryRouter } from "react-router-dom"; // Import MemoryRouter
 
 test("renders form", () => {
-  <MemoryRouter>
-    render(<SearchIncidents />);
-  </MemoryRouter>
+  render(
+    <MemoryRouter>
+      <SearchIncidents />
+    </MemoryRouter>
+  );
 });
 
 test("displays search criteria dropdown", () => {
-  const { container } = <MemoryRouter>render(<SearchIncidents />)</MemoryRouter>;
+  const { container } = (
+    <MemoryRouter>
+      <SearchIncidents />
+    </MemoryRouter>
+  );
   const searchInput = container.querySelector("#search-criteria");
   expect(searchInput).toBeInTheDocument();
 });
 
 test("displays search input field", () => {
-  const { container } = <MemoryRouter>render(<SearchIncidents />)</MemoryRouter>;
+  const { container } = (
+    <MemoryRouter>
+      <SearchIncidents />
+    </MemoryRouter>
+  );
   const searchInput = container.querySelector("#search-term");
   expect(searchInput).toBeInTheDocument();
 });
 
 test("updates search term when typing", () => {
-  const { container } = <MemoryRouter>render(<SearchIncidents />)</MemoryRouter>;
+  const { container } = (
+    <MemoryRouter>
+      <SearchIncidents />
+    </MemoryRouter>
+  );
   const searchInput = container.querySelector("#search-term");
 
   fireEvent.change(searchInput, { target: { value: "Test Search Term" } });
