f30c354cd7f5ebd9ce2c5f6830f98ed98b26c004
Derrick <xreamic@gmail.com>
2023-10-12 15:00:36 +1100

    Update searchForms.test.js

--- src/Tests/searchForms.test.js
+++ src/Tests/searchForms.test.js
@@ -16,19 +16,31 @@ test("renders form", () => {
 });
 
 test("displays search criteria dropdown", () => {
-  const { container } = render(<SearchIncidents />);
+  const { container } = render(
+    <MemoryRouter>
+      <SearchIncidents />
+    </MemoryRouter>
+  );
   const searchInput = container.querySelector("#search-criteria");
   expect(searchInput).toBeInTheDocument();
 });
 
 test("displays search input field", () => {
-  const { container } = render(<SearchIncidents />);
+  const { container } = render(
+    <MemoryRouter>
+      <SearchIncidents />
+    </MemoryRouter>
+  );
   const searchInput = container.querySelector("#search-term");
   expect(searchInput).toBeInTheDocument();
 });
 
 test("updates search term when typing", () => {
-  const { container } = render(<SearchIncidents />);
+  const { container } = render(
+    <MemoryRouter>
+      <SearchIncidents />
+    </MemoryRouter>
+  );
   const searchInput = container.querySelector("#search-term");
 
   fireEvent.change(searchInput, { target: { value: "Test Search Term" } });
