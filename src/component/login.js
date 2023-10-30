33e26b8f9bd516da68ff30722349476d821f6097
byro23 <byron.j.lester@student.uts.edu.au>
2023-10-31 10:34:11 +1100

    revert change

--- src/component/login.js
+++ src/component/login.js
@@ -26,7 +26,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch("https://incident-reporting-system.azurewebsites.net/api/login", {
+      const response = await fetch("http://localhost:4000/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
